8baba2465c7849219d9fb698b55c1405b2d20ad7
jadon-vish <147917777+jadon-vish@users.noreply.github.com>
2023-10-17 19:24:00 +0000

    conversions and operations

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,31 @@
+let score="true"
+
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber=Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1 , fasle => 0
+//undefined =>NaN
+
+let isLoggedIn="1"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+//1 => true , 0=>fasle
+//"" => false
+//"something" =>true
+
+let sumNumber= 33
+
+let stringNumber=String(sumNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
