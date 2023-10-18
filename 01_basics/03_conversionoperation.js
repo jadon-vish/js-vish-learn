3f681bcdb58a30bbd9733837d0ede312eb697303
jadon-vish <147917777+jadon-vish@users.noreply.github.com>
2023-10-18 09:33:05 +0000

    comparision and conversion of datatypes

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -18,7 +18,7 @@ let isLoggedIn="1"
 
 let booleanIsLoggedIn=Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 //1 => true , 0=>fasle
 //"" => false
@@ -27,5 +27,28 @@ console.log(booleanIsLoggedIn)
 let sumNumber= 33
 
 let stringNumber=String(sumNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//*******************operations***************
+let value=3
+let negValue= -value
+//console.log(negValue)
+
+let str= "Hello "
+let str2="Vishesh"
+
+let str3=str+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2") whichever is first will convert whole statement
+
+//console.log(+true)
+//console.log(+"")
+
+let gameCounter=100
+//console.log(gameCounter)
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,16 @@
+//Main focus on comparision of different data types
+//console.log("2">1)
+
+// console.log(null >0) we dont get predictable result bcz null gets converted sometimes into NaN or 0
+// console.log(null<=0)
+// console.log(null == 0)
+
+//console.log(undefined == 0)
+
+// === -> checks data types as well
+console.log(2=="2")
+console.log(2 === "2")
+
+
+// NOTE: always avoid such codes to avoid non predictability
+
