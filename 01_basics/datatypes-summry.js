3087b6312498a266b8d583b2367da0fcf9cb78ec
jadon-vish <147917777+jadon-vish@users.noreply.github.com>
2023-10-22 15:38:02 +0000

    datatypse-summary

---
+++ 01_basics/datatypes-summry.js
@@ -0,0 +1,26 @@
+// #Primitive datatypes: 7 categories - these are callbyvalue 
+
+//7 types: String,Number,boolean,null,undefined,symbol,bigint.
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+//console.log(id===anotherId)
+
+const bigNumber=234343545454554n
+
+
+
+
+
+
+//Reference type/ non primitive:
+//objects , arrays , functions
